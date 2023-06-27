2343f799d2318e6c9310bddf9aa46d5cc79c4f8e
RAWINI SATHEESHA <134356654+RAWINISATHEESHA@users.noreply.github.com>
2023-06-27 09:56:23 +0530

    Add files via upload

---
+++ LAB6-Q2/LAB6-Q2/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = { 5, 6, 3, 7, 9, 8, 4, 2, 1 };
+
+int minimum = FindMinimum(numbers);
+int maximum = FindMaximum(numbers);
+
+Console.WriteLine($"MINIMUM VALUE :{minimum}");
+Console.WriteLine($"MAXIMUM VALUE :{maximum}");
+
+Console.WriteLine("  PRESS ANY KEY TO EXIT..");
+Console.ReadKey();
+
+static int FindMinimum(int[] array)
+{
+    int min = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+    if (array[i] < min)
+        {
+            min = array[i];
+
+        }
+    }
+    return min;
+
+}
+static int FindMaximum(int[] array)
+{
+    int max = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+
+        }
+    }
+    return max;
+
+}
+
+
+
+
