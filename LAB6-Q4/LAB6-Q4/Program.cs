8dd430464f6a6f08fce04e8c62921a4dc8b55f13
RAWINI SATHEESHA <134356654+RAWINISATHEESHA@users.noreply.github.com>
2023-06-27 10:57:40 +0530

    Add files via upload

---
+++ LAB6-Q4/LAB6-Q4/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = new int[20];
+
+Random random = new Random();
+
+for (int i = 0; i < numbers.Length; i++)
+
+{
+    numbers[i] = random.Next(1, 100);
+}
+
+int min = numbers[0];
+int max = numbers[0];
+
+for ( int i = 1; i < numbers.Length; i++)
+{
+if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+
+    }
+}
+
+
+Console.WriteLine("ARRAY : " + string.Join(", ", numbers));
+Console.WriteLine("MINIMUM VALUE: " + min);
+Console.WriteLine("MAXIMUM VALUE: " + max);
+
