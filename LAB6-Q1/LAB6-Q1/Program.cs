719930150dee835dbd2c7d0333ab723c5d703c15
RAWINI SATHEESHA <134356654+RAWINISATHEESHA@users.noreply.github.com>
2023-06-27 09:30:38 +0530

    Add files via upload

---
+++ LAB6-Q1/LAB6-Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = new int[10];
+
+Console.WriteLine("ENTER 10 NUMBERS");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"NUMBER {i + 1}:");
+    numbers[i] = int.Parse(Console.ReadLine());
+
+}
+
+int sum = GetScalarSum(numbers);
+Console.WriteLine($"Scalar Sum :{sum}");
+
+Console.WriteLine(" PRESS ANY KEY TO EXIT..");
+Console.ReadLine();
+
+static int GetScalarSum(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
