0a251970d718545733c75ca15278df925db9ae5a
RAWINI SATHEESHA <134356654+RAWINISATHEESHA@users.noreply.github.com>
2023-07-04 11:22:34 +0530

    Add files via upload

---
+++ PRACTICAL6-Q6/PRACTICAL6-Q6/ArrayClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PRACTICAL6_Q6
+{
+	public class ArrayClass
+	{
+		public int[] arrayMethod(int size)
+		{
+			int[] array = new int[size];
+
+			for (int i = 0; i < array.Length; i += 2)
+			{
+				Console.WriteLine("ENTER A VALUE:");
+				int value = Convert.ToInt32(Console.ReadLine());
+				array[i] = value;
+				array[i + 1] = 0;
+
+			}
+			return array;
+		}
+
+	}
+}
+
---
+++ PRACTICAL6-Q6/PRACTICAL6-Q6/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("ENTER  THE SIZE OF THE ARRAY:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+ArrayClass obj1 = new ArrayClass();
+int[] array = obj1.arrayMethod(size);
+
+Console.WriteLine("\nDisplay the array:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i] + " ");
+}
+
+Console.ReadLine();
+
