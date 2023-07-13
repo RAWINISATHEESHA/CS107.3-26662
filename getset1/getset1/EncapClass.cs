a2c698e67a7c82f731b4614159ae4becf1d30f11
RAWINI SATHEESHA <134356654+RAWINISATHEESHA@users.noreply.github.com>
2023-07-13 14:07:13 +0530

    Add files via upload

---
+++ getset1/getset1/EncapClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace getset1
+{
+	public class EncapClass
+	{
+        private int age;
+        private string name;
+
+        public void setAge(int ageFromUser)
+           
+        {
+            age = ageFromUser;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+        public void setName(string nameFromUser)
+
+        {
+            name = nameFromUser;
+        }
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
---
+++ getset1/getset1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information;
+using getset1;
+
+EncapClass ec = new EncapClass();
+
+ec.setAge(25);
+Console.WriteLine("YOUR AGE IS " + ec.getAge());
+Console.ReadLine();
+
+ec.setName("TAYLOR");
+Console.WriteLine("YOUR NAME IS " + ec.getName());
+Console.ReadLine();
+
+
+
